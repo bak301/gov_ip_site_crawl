c4444b6677a3babb3ea011266aaf409456f39704
khanhln <soulbodyx@gmail.com>
2024-04-22 02:12:40 +0700

    fix bug

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const fs = require("fs");
 const base_url =
   "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/ajax/detail/trademarks?id=";
 let dataFile = "ID_Trademark.csv";
-
+let data = fs.readFileSync("ID_Trademark.csv", "utf-8");
+let array = data.split("\n");
 async function start() {
-  setTimeout(async () => {
-    let data = fs.readFileSync("ID_Trademark.csv", "utf-8");
-    let array = data.split("\n");
-    for (let i = 0; i < LIMIT; i++) {
-      let response = await fetch(base_url + array[i]);
-      let status = response.status;
-      let text = await response.text();
+  recurse_request(0);
+}
 
-      console.log("ID " + array[i] + " : Response " + status);
-      fs.appendFileSync(
-        "result.txt",
-        response.text + "\n--------------------------------------------------\n"
-      );
-    }
-  }, 1000);
+function recurse_request(i) {
+  if (i < LIMIT) {
+    fetch(base_url + array[i])
+      .then((response) => {
+        let status = response.status;
+        console.log("ID :" + array[i]);
+        console.log("\tHTTP Response :  " + status);
+        return response.text();
+      })
+      .then((text) => {
+        fs.appendFileSync(
+          "result.txt",
+          text + "\n--------------------------------------------------\n"
+        );
+        recurse_request(i + 1); // Fixed increment to i + 1
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    console.log("request completed");
+  }
 }
 
 start();
