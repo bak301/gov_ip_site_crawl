19ed87bbeceac5195e7eae6a169ababd8934b5aa
khanhln <soulbodyx@gmail.com>
2024-04-23 14:41:33 +0700

    add filter to data output

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const LIMIT = 2;
-const thread_count = 2;
+const LIMIT = 10;
+const thread_count = 5;
 
 const fs = require("fs");
+const { JSDOM } = require("jsdom");
 const base_url =
   "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/ajax/detail/trademarks?id=";
 let dataFile = "ID_Trademark.csv";
@@ -41,10 +42,8 @@ function recurse_request(i) {
         if (text.includes("An unexpected server error has occurred")) {
           console.log("Job number " + i + "has failed !\n");
         } else {
-          fs.appendFileSync(
-            "result.html",
-            text + "\n--------------------------------------------------\n"
-          );
+          let outputData = cleanHtml(text);
+          fs.appendFileSync("result.html", outputData);
           recurse_request((i += thread_count));
         }
       })
@@ -56,6 +55,20 @@ function recurse_request(i) {
   }
 }
 
-function cleanHtml(text) {}
+function cleanHtml(text) {
+  let accordions = new JSDOM(text).window.document.querySelectorAll(
+    "#accordion-1a, #accordion-2a, #accordion-3a"
+  );
+  let cleanedHtml = "";
+
+  accordions.forEach((accordion) => {
+    const id = accordion.id; // Extract ID to determine the index
+    const i = parseInt(id.substring(id.lastIndexOf("-") + 1, id.length - 1)); // Extract the index from the ID
+    cleanedHtml += `<div class='accordion-${i}a'>${accordion.innerHTML}</div>`;
+  });
+
+  cleanedHtml += "\n---------------\n";
+  return cleanedHtml;
+}
 
 start();
