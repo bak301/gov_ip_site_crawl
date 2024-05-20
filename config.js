d3c405476988c84e2ee795275e2dff4bb04b4ce9
Trinh Bach <soulbodyx@gmail.com>
2024-05-20 20:38:03 +0700

    add retry options. if failed after N retry, move on to next job

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    BASE_URL : "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/ajax/detail/trademarks?id=",
+    DATA_FILE : "ID_Trademark.csv",
+    RETRY_LIMIT : 3,
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const LIMIT = 10;
+const LIMIT = 20;
 const thread_count = 5;
 
 const fs = require("fs");
-const { JSDOM } = require("jsdom");
-const base_url =
-  "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/ajax/detail/trademarks?id=";
-let dataFile = "ID_Trademark.csv";
-let data = fs.readFileSync("ID_Trademark.csv", "utf-8");
-let array = data.split("\n");
+const JSDOM = require("jsdom").JSDOM;
+const config = require("./config.js");
+
+let IDs = fs.readFileSync(config.DATA_FILE, "utf-8").split("\n");
 
 async function start() {
   for (let index = 0; index < thread_count; index++) {
@@ -15,32 +13,28 @@ async function start() {
   }
 }
 
-function recurse_request(i) {
+function recurse_request(i, retryCount = 0) {
   if (i < LIMIT) {
     let startTime = performance.now();
-    fetch(base_url + array[i])
+    fetch(config.BASE_URL + IDs[i])
       .then((response) => {
-        let endTime = performance.now();
-
-        console.log(
-          "\nJob number " +
-            i +
-            " has been finished at " +
-            new Date(Date.now()).toLocaleString("vi-VN")
-        );
-        console.log(
-          "HTTP Status : " + response.status + " : " + response.statusText
-        );
-        console.log("ID :" + array[i]);
-        console.log(
-          "Finish time : " + ((endTime - startTime) / 1000).toFixed(2) + "s"
-        );
-
+        logJobDetails(i, response, IDs, startTime);
         return response.text();
       })
       .then((text) => {
         if (text.includes("An unexpected server error has occurred")) {
-          console.log("Job number " + i + "has failed !\n");
+          if (retryCount < config.RETRY_LIMIT) {
+            console.log(
+              `Retrying job number ${i}... Attempt ${retryCount + 1}`
+            );
+            recurse_request(i, retryCount + 1);
+          } else {
+            console.log(
+              `Job number ${i} has failed after ${config.RETRY_LIMIT} attempts. . Moving on ....\n`
+            );
+            fs.appendFileSync("error.html", `ID ${IDs[i].trim()} has failed !`);
+            recurse_request((i += thread_count));
+          }
         } else {
           let outputData = cleanHtml(text);
           fs.appendFileSync("result.html", outputData);
@@ -69,6 +63,15 @@ function cleanHtml(text) {
 
   cleanedHtml += "\n---------------\n";
   return cleanedHtml;
+}
+
+function logJobDetails(i, response, IDs, startTime) {
+  let endTime = performance.now();
+  let currentTime = new Date(Date.now()).toLocaleString("vi-VN");
+  console.log(`\nJob number ${i} has been finished at ${currentTime}`);
+  console.log(`HTTP Status : ${response.status} : ${response.statusText}`);
+  console.log("ID :" + IDs[i]);
+  console.log(`Finish time : ${((endTime - startTime) / 1000).toFixed(2)}s`);
 }
 
 start();
