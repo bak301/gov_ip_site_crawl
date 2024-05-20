0bef8ea4b60340f07fbf6b7445f171f7c9b889dd
Trinh Bach <soulbodyx@gmail.com>
2024-05-20 20:52:50 +0700

    refractor code

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 const config = {
     BASE_URL : "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/ajax/detail/trademarks?id=",
-    DATA_FILE : "ID_Trademark.csv",
+    DATA_PATH : "ID_Trademark.csv",
     RETRY_LIMIT : 3,
+    delay : {
+        BETWEEN_REQUEST : 0
+    },
+    string : {
+        INTERNAL_SERVER_ERROR : "An unexpected server error has occurred"
+    }
 }
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,63 +1,64 @@
 const LIMIT = 20;
 const thread_count = 5;
-
 const fs = require("fs");
 const JSDOM = require("jsdom").JSDOM;
 const config = require("./config.js");
-
-let IDs = fs.readFileSync(config.DATA_FILE, "utf-8").split("\n");
+const IDs = fs.readFileSync(config.DATA_PATH, "utf-8").split("\n");
 
 async function start() {
+  const promises = [];
   for (let index = 0; index < thread_count; index++) {
-    recurse_request(index);
+    promises.push(recurse_request(index));
+  }
+  await Promise.all(promises);
+}
+
+async function recurse_request(i, retryCount = 0) {
+  if (i >= LIMIT) {
+    console.log("request completed");
+    return;
+  }
+
+  let startTime = performance.now();
+  try {
+    const response = await fetch(config.BASE_URL + IDs[i]);
+    logJobDetails(i, response, IDs, startTime);
+    const text = await response.text();
+
+    if (text.includes(config.string.INTERNAL_SERVER_ERROR)) {
+      await handleServerError(i, retryCount);
+    } else {
+      extractDataThenContinue(i, text);
+    }
+  } catch (error) {
+    console.error("Error while fetch:", error);
   }
 }
 
-function recurse_request(i, retryCount = 0) {
-  if (i < LIMIT) {
-    let startTime = performance.now();
-    fetch(config.BASE_URL + IDs[i])
-      .then((response) => {
-        logJobDetails(i, response, IDs, startTime);
-        return response.text();
-      })
-      .then((text) => {
-        if (text.includes("An unexpected server error has occurred")) {
-          if (retryCount < config.RETRY_LIMIT) {
-            console.log(
-              `Retrying job number ${i}... Attempt ${retryCount + 1}`
-            );
-            recurse_request(i, retryCount + 1);
-          } else {
-            console.log(
-              `Job number ${i} has failed after ${config.RETRY_LIMIT} attempts. . Moving on ....\n`
-            );
-            fs.appendFileSync("error.html", `ID ${IDs[i].trim()} has failed !`);
-            recurse_request((i += thread_count));
-          }
-        } else {
-          let outputData = cleanHtml(text);
-          fs.appendFileSync("result.html", outputData);
-          recurse_request((i += thread_count));
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+async function handleServerError(i, retryCount) {
+  if (retryCount < config.RETRY_LIMIT) {
+    console.log(`Retrying job number ${i}... Attempt ${retryCount + 1}`);
+    await recurse_request(i, retryCount + 1);
   } else {
-    console.log("request completed");
+    console.log(`Job number ${i} has failed after ${config.RETRY_LIMIT} attempts. Moving on ....\n`);
+    fs.appendFileSync("error.html", `ID ${IDs[i].trim()} has failed !\n`);
+    await recurse_request((i += thread_count));
   }
+}
+
+function extractDataThenContinue(i, text) {
+  let outputData = cleanHtml(text);
+  fs.appendFileSync("result.html", outputData);
+  recurse_request((i += thread_count));
 }
 
 function cleanHtml(text) {
-  let accordions = new JSDOM(text).window.document.querySelectorAll(
-    "#accordion-1a, #accordion-2a, #accordion-3a"
-  );
+  let accordions = new JSDOM(text).window.document.querySelectorAll("#accordion-1a, #accordion-2a, #accordion-3a");
   let cleanedHtml = "";
 
   accordions.forEach((accordion) => {
-    const id = accordion.id; // Extract ID to determine the index
-    const i = parseInt(id.substring(id.lastIndexOf("-") + 1, id.length - 1)); // Extract the index from the ID
+    const id = accordion.id;
+    const i = parseInt(id.substring(id.lastIndexOf("-") + 1, id.length - 1));
     cleanedHtml += `<div class='accordion-${i}a'>${accordion.innerHTML}</div>`;
   });
 
